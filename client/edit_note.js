876ddecd4420a28549f24006f3f50e478abb1659
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2015-10-29 23:16:00 +0200

    Fixed a bug: autosave functionality clears after the user leaves the edit note page

--- client/edit_note.js
+++ client/edit_note.js
@@ -34,41 +34,7 @@ Template.editNote.helpers({
 Template.editNote.events({
   'click .updateNote': function(e){
     e.preventDefault();
-    var title = $('#title').val();
-    var category = $('#category').val();
-    var noteContent = $('#summernote').code();
-    var error = 0;
-    if(!(title.length > 4)){
-      error = 1;
-    };
-
-    if(category == 0){
-      error = 1;
-    };
-
-    if(noteContent == 0){
-      error = 1;
-    }
-
-    if(error == 0){
-      var note = {};
-      note.title = title;
-      note.category = category;
-      note.content = noteContent;
-      var noteId = e.currentTarget.id;
-      Meteor.call("updateNote", noteId, note, function(error, result){
-        if(error){
-          alert(error);
-        }else{
-          $('.alert-success').show("slow");
-          Meteor.setTimeout(function(){
-            $('.alert-success').hide("slow");
-          }, 2000);
-        }
-      });
-    }else{
-      alert("All fields have to be completed!");
-    }
+    save();
   }
 });
 
@@ -82,44 +48,51 @@ Template.editNote.onRendered(function(){
     Session.set("numberOfWords", countTheWords(noteContent).wordsCount);
     Session.set("numberOfPages", countTheWords(noteContent).pages);
   }, 500);
-
-  // autosave the note every 3 minutes
-  Meteor.setInterval(function(){
-    var title = $('#title').val();
-    var category = $('#category').val();
-    var noteContent = $('#summernote').code();
-    var error = 0;
-    if(!(title.length > 4)){
-      error = 1;
-    };
-
-    if(category == 0){
-      error = 1;
-    };
-
-    if(noteContent == 0){
-      error = 1;
-    }
-
-    if(error == 0){
-      var note = {};
-      note.title = title;
-      note.category = category;
-      note.content = noteContent;
-      var noteId = Router.current().params._id;
-      Meteor.call("updateNote", noteId, note, function(error, result){
-        if(error){
-          console.log(error);
-        }else{
-          $('.autosave').show(500);
-          Meteor.setTimeout(function(){
-            $('.autosave').hide(500);
-          }, 3000);
-        }
-      });
-    }else{
-      alert("All fields have to be completed!");
-    }
-
-  }, 180000);
 });
+
+Template.editNote.onCreated(function(){
+  // autosave the note every 3 minutes
+  theInterval = Meteor.setInterval(save, 180000);
+});
+
+Template.editNote.onDestroyed(function(){
+  Meteor.clearInterval(theInterval);
+});
+
+function save(){
+  var title = $('#title').val();
+  var category = $('#category').val();
+  var noteContent = $('#summernote').code();
+  var error = 0;
+  if(!(title.length > 4)){
+    error = 1;
+  };
+
+  if(category == 0){
+    error = 1;
+  };
+
+  if(noteContent == 0){
+    error = 1;
+  }
+
+  if(error == 0){
+    var note = {};
+    note.title = title;
+    note.category = category;
+    note.content = noteContent;
+    var noteId = Router.current().params._id;
+    Meteor.call("updateNote", noteId, note, function(error, result){
+      if(error){
+        console.log(error);
+      }else{
+        $('.autosave').show(500);
+        Meteor.setTimeout(function(){
+          $('.autosave').hide(500);
+        }, 3000);
+      }
+    });
+  }else{
+    alert("All fields have to be completed!");
+  }
+}
