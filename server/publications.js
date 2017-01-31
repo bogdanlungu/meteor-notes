ecb007df7881cc3a25a0c6bcf2a6cf7ffac3f0b2
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2017-01-31 23:07:18 +0200

    Editorconfig and some formatting

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,9 @@
 if (Meteor.isServer) {
-  Meteor.publish('categories', function() {
-    return Categories.find( {uId: this.userId} );
+  Meteor.publish('categories', function () {
+    return Categories.find({ uId: this.userId });
   });
 
-  Meteor.publish('notes', function() {
-    return Notes.find( {uId: this.userId} );
+  Meteor.publish('notes', function () {
+    return Notes.find({ uId: this.userId });
   });
 }
